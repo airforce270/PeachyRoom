42e88a267199210f94dedfbd58ef44a87f74aa6c
ItMePeachy <106801886+ItMePeachy@users.noreply.github.com>
2024-04-07 20:57:29 +0000

    Deploying to deploy from @ ItMePeachy/PeachyRoom@e8d5b2f45b13bcbba09d048d34540999321a954c 🚀

--- deploy/beta/main.js
+++ deploy/beta/main.js
@@ -3583,7 +3583,7 @@ var cydj = (function (exports) {
       35,
     ],
     [
-      ':blahaj:',
+      ':skeet:',
       'https://cdn.7tv.app/emote/62740ee56fc5e131afe3d18e/1x.webp',
       57,
       32,
