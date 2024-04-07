e7aca7dec3711605f9b1a626e73a1554a51de396
ItMePeachy <106801886+ItMePeachy@users.noreply.github.com>
2024-04-07 22:44:03 +0000

    Deploying to deploy from @ ItMePeachy/PeachyRoom@7abf0c8dea99da4b4cf96333c120e6e88aec450c 🚀

--- deploy/main/main.js
+++ deploy/main/main.js
@@ -4555,13 +4555,13 @@ var cydj = (function (exports) {
       background-color: #383E40;
   }
 
- .btn {
-      background-image: linear-gradient(#4c333e00,#4c333e00 60%,#4c333e00)!important;
-      border-color: #4cb290!important
+  .btn {
+    background-image: linear-gradient(#4c333e00,#4c333e00 60%,#4c333e00)!important;
+      border-color: #ffbbf9!important
   }
 
   .btn-default:hover {
-    background-color: #4cb290
+      background-color: #ffbbf9
   }
 
   .modal-body {
@@ -4581,11 +4581,11 @@ var cydj = (function (exports) {
   }
 
   #motd {
-      background-color: #1cb0ff
+      background-color: #fff0
   }
 
   #motdwrap {
-      background-color: rgb(124 212 255 / 40%);
+      background-color: #283544;
   }
 
   #userlist {
