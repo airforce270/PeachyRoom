bc94c6869fdd1e9353cb910ca42d707914a04402
ItMePeachy <106801886+ItMePeachy@users.noreply.github.com>
2024-04-07 20:57:38 +0000

    Deploying to deploy from @ ItMePeachy/PeachyRoom@e8d5b2f45b13bcbba09d048d34540999321a954c 🚀

--- deploy/chocomint/main.js
+++ deploy/chocomint/main.js
@@ -3443,7 +3443,7 @@ var cydj = (function (exports) {
 
   const RulesBtn_Caption = 'Read Channel Rules';
 
-  const TitleIcon_URL = 'https://cdn.7tv.app/emote/6040a8bccf6746000db10348/2x.webp';
+  const TitleIcon_URL = 'https://cdn.7tv.app/emote/64115dcabdbc1698547639ac/2x.webp';
 
   const TitleBarDescription_Caption = 'Now Playing:';
 
@@ -3453,20 +3453,15 @@ var cydj = (function (exports) {
 
   const CustomPingSound_URL = 'https://github.com/ItMePeachy/PeachyRoom/raw/master/misc/squeak.mp3';
 
-  const PlayerHiding_URL = 'https://c.tenor.com/Q6UjBrnSzvQAAAAC/anime-uh.gif';
+  const PlayerHiding_URL = 'https://media1.tenor.com/m/hrIx2VHU5CAAAAAC/kitty-cat.gif';
 
   const HeaderDropMenu_Array = [
-    ['CyDJ Rooms', ''],
-    ['Main Room', 'https://cytu.be/r/cydj'],
-    ['Second Room', 'https://cytu.be/r/secretfbimeeting'],
-    ['Baked Live Room', 'https://baked.live/tv/cydj'],
-    ['Test Room', 'https://cytu.be/r/testplacelilroc'],
-    ['Test Room 2', 'https://cytu.be/r/emptyroomtestplace'],
-    ['Test Room 3', 'https://cytu.be/r/xqcPeepo'],
-    ['Experiments', 'https://cytu.be/r/cydjrewrite'],
+    [`Peachy's Rooms`, ''],
+    ['Main Room', 'https://cytu.be/r/peachyroom'],
+    ['Beta Room', 'https://cytu.be/r/peachyBeta'],
     ['Community Related', ''],
-    ['CyDJ Discord', 'https://discord.gg/g8tCGSc2bx'],
-    ['Camellia Discord', 'https://discord.gg/camellia'],
+    [`Peachy's Discord', 'https://discord.gg/cMUXkDcX6C`],
+    ['CyDJ Room', 'https://cytu.be/r/CyDJ'],
     ['Credits', ''],
     [
       'CyTube FAQ',
@@ -3474,7 +3469,8 @@ var cydj = (function (exports) {
     ],
     ['CyTube Source', 'https://github.com/calzoneman/sync'],
     ['CyDJ Source', 'https://github.com/papertek/CyDJ'],
-    ['CyDJ Bot Source', 'https://github.com/airforce270/CytubeBot'],
+    ['PeachyRoom Source', 'https://github.com/ItMePeachy/PeachyRoom'],
+    ['CyDJ Old MrDestrucoid Bot Source', 'https://github.com/airforce270/CytubeBot'],
   ];
 
   const ModPanel_Array = [
@@ -3564,19 +3560,6 @@ var cydj = (function (exports) {
     ],
   ];
 
-  const EmptyCornerBackground = [
-    'https://static-cdn.jtvnw.net/emoticons/v2/emotesv2_a054f4001b6d4f098e7969c988debd18/default/light/2.0',
-    'https://cdn.betterttv.net/emote/5dfc5d868608fb0da4120b59/2x',
-    'https://raw.githubusercontent.com/papertek/CyDJ/beta/images/main/891194776798498826%20(1).gif',
-    'https://raw.githubusercontent.com/papertek/CyDJ/beta/images/main/small.png',
-    'https://cdn.7tv.app/emote/60d2c62291b6751bc1e05add/4x.webp',
-    'https://raw.githubusercontent.com/papertek/CyDJ/beta/images/main/JohnJamSmall.gif',
-    'https://raw.githubusercontent.com/papertek/CyDJ/beta/images/main/mud.png',
-    'https://raw.githubusercontent.com/papertek/CyDJ/beta/images/main/xso.png',
-    'https://raw.githubusercontent.com/papertek/CyDJ/beta/images/main/pacific.gif',
-    'https://raw.githubusercontent.com/papertek/CyDJ/beta/images/main/pushing%20ass.png',
-  ];
-
   // /////////////////////////////////////////////////////////////////////////////////////////////////
 
   /* ----- INDEPENDENT EMOTES AND FILTERS CONFIGURATION ----- */
@@ -3601,9 +3584,9 @@ var cydj = (function (exports) {
     ],
     [
       ':skeet:',
-      'https://raw.githubusercontent.com/papertek/CyDJ/beta/images/main/skeet.gif',
-      35,
-      35,
+      'https://cdn.7tv.app/emote/62740ee56fc5e131afe3d18e/1x.webp',
+      57,
+      32,
     ],
   ];
 
@@ -5912,15 +5895,6 @@ var cydj = (function (exports) {
         fontsbtnwrap.append('<br />');
       }
     }
-  }
-
-  // adding background image to empty playlistrow corner
-  if (EmptyCornerBackground.length > 0) {
-    const rnd = Math.round(Math.random() * (EmptyCornerBackground.length - 1));
-    $('#playlistrow').css({
-      'background-image': `url("${EmptyCornerBackground[rnd]}")`,
-      'background-repeat': 'no-repeat',
-    });
   }
 
   // adding layout configuration panel button
